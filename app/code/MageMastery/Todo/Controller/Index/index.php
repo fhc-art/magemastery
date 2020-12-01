9b63b3acf739878d509eae1c26820d81d12eb26f
fhc-art <art@fhc-usa.com>
2020-11-30 20:14:26 -0800

    Magemastery lesson 4

---
+++ app/code/MageMastery/Todo/Controller/Index/index.php
@@ -0,0 +1,12 @@
+<?php
+namespace MageMastery\Todo\Controller\Index
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute ()
+{
+    return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+}
+}
